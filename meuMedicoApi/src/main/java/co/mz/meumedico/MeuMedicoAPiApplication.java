71444601c3fc5a2c0a0af79a74b8267f4e0afe08
Luiz dos Santos Tique Júnior <38396043+Tiquinho10@users.noreply.github.com>
2023-09-20 04:17:31 +0200

    adding environment configuration

---
+++ meuMedicoApi/src/main/java/co/mz/meumedico/MeuMedicoAPiApplication.java
@@ -0,0 +1,13 @@
+package co.mz.meumedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuMedicoAPiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeuMedicoAPiApplication.class, args);
+    }
+
+}
---
+++ meuMedicoApi/src/test/java/co/mz/meumedico/MeuMedicoAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.mz.meumedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuMedicoAPiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
