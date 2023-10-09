f49f8feca1ed91bd032c74083177007335f11e29
elisioMonjane <104235091+elisioMonjane@users.noreply.github.com>
2023-10-10 00:16:31 +0200

    Add files via upload

---
+++ painelUtente.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="sidebar">
+      <ul>
+        <li><router-link to="/">Página Inicial</router-link></li>
+        <li><router-link to="/dashboard">Dashboard</router-link></li>
+        <li><router-link to="/perfil">Perfil</router-link></li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Sidebar",
+  };
+  </script>
+  
+  <style scoped>
+  .sidebar {
+    width: 250px;
+    background-color: #333;
+    color: #fff;
+    padding: 20px;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    margin-bottom: 10px;
+  }
+  
+  a {
+    text-decoration: none;
+    color: #f3e6e6;
+  }
+  </style>
+  
